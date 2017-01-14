ec006e1227e1b8cec190c593a0e488244889e8e9
lifengsofts <lifengsofts@gmail.com>
2017-01-14 18:38:51 +0800

    add init project

---
+++ simple/src/main/java/cn/woblog/android/downloader/simple/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.woblog.android.downloader.simple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
