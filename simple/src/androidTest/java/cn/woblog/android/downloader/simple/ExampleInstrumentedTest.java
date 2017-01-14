205da272eea3b4df02b62a84aab81042ed0b78d8
lifengsofts <lifengsofts@gmail.com>
2017-01-14 18:58:51 +0800

    add google-style

--- simple/src/androidTest/java/cn/woblog/android/downloader/simple/ExampleInstrumentedTest.java
+++ simple/src/androidTest/java/cn/woblog/android/downloader/simple/ExampleInstrumentedTest.java
@@ -1,13 +1,12 @@
 package cn.woblog.android.downloader.simple;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +15,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cn.woblog.android.downloader.simple", appContext.getPackageName());
-    }
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("cn.woblog.android.downloader.simple", appContext.getPackageName());
+  }
 }
