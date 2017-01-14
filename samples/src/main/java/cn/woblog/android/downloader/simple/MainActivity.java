cb23c5f247b70648add909b130a1505f163a3540
lifengsofts <lifengsofts@gmail.com>
2017-01-15 01:07:08 +0800

    add check style

--- samples/src/main/java/cn/woblog/android/downloader/simple/MainActivity.java
+++ samples/src/main/java/cn/woblog/android/downloader/simple/MainActivity.java
@@ -3,6 +3,9 @@ package cn.woblog.android.downloader.simple;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * sample main activity.
+ */
 public class MainActivity extends AppCompatActivity {
 
   @Override
