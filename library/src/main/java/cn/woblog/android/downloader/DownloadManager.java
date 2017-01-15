1fef3d99432f48e8ea3397ee74149586bf79118c
lifengsofts <lifengsofts@gmail.com>
2017-01-15 19:27:09 +0800

    add basic download framework

--- library/src/main/java/cn/woblog/android/downloader/DownloadManager.java
+++ library/src/main/java/cn/woblog/android/downloader/DownloadManager.java
@@ -1,9 +1,51 @@
 package cn.woblog.android.downloader;
 
+import android.content.Context;
+import cn.woblog.android.downloader.callback.IDownloadManager;
+import cn.woblog.android.downloader.domain.DownloadInfo;
+
 /**
  * Created by renpingqing on 14/01/2017.
  */
 
-public class DownloadManager {
+public class DownloadManager implements IDownloadManager {
 
+  private static DownloadManager instance;
+
+  public DownloadManager(Context context) {
+
+  }
+
+  public static DownloadManager getInstance(Context context) {
+    synchronized (DownloadManager.class) {
+      if (instance == null) {
+        instance = new DownloadManager(context);
+      }
+    }
+    return instance;
+  }
+
+  public void onDestroy() {
+
+  }
+
+  @Override
+  public void download(DownloadInfo info) {
+
+  }
+
+  @Override
+  public void pause(DownloadInfo info) {
+
+  }
+
+  @Override
+  public void resume(DownloadInfo info) {
+
+  }
+
+  @Override
+  public void remove(DownloadInfo info) {
+
+  }
 }
